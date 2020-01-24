739c79ec053ae9e209745014ec1be2e2357425a0
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-01-23 23:27:40 -0800

    [A] Add basic bot - allow to return gearing data with player input - [A] Add handling error

---
+++ src/log.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+log = (input) => {
+
+    let time = Date()
+    let data = `${time} - ${input.author} - "${input.log}"\n`;
+
+    fs.appendFile("./log", data, (err) => {
+        if (err) throw err;
+    });        
+}
+
+const messageArg = `
+Pour utiliser ***Gearing-HCR2*** :
+\`\`\`
+    ![type] [niveau] [nombre de pièce actuelle]
+
+    type : 
+        common - commun - classique
+        rare - gold - or
+        epic - epique
+        legendary - legendaire
+
+    niveau :
+        niveau de l'amélioration
+    
+    nombre de pièce :
+        Le nombre que vous voyez affiché sur votre pièce
+\`\`\`
+Exemple : 
+\`\`\`!epic 4 21\`\`\`
+`;
+
+exports.log = log;
+exports.messageArg = messageArg;
